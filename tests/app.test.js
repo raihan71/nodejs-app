6f24765de664baa8860f8174a4b3018fe050437f
Raihan Nismara <raihannismara@gmail.com>
2025-05-30 19:19:56 +0700

    chore: update testing schema

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,8 +1,5 @@
-const app = require('../app');
-const request = require('supertest');
-
-describe('testing app.js', () => {
-  test('should return hello world', () => {
-    request(app).get('/').expect(200).expect('Hello World!');
+describe('testing aritmatika', () => {
+  test('1+1 should be 2', () => {
+    expect(1 + 1).toBe(2);
   });
 });
