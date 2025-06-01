05dbf2866fdd0a6b7fbd959b40e3c8def5a3de8b
Raihan Nismara <raihannismara@gmail.com>
2025-06-01 08:16:53 +0700

    chore: update port app

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 3000;
 
 app.disable('x-powered-by');
 app.use(express.static(path.join(__dirname, 'public')));
